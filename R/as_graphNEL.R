3b6e1296c72f7a03f5b6a7d4a383fa7830b10baf
alesmascaro <a.mascaro3@campus.unimib.it>
2023-12-31 18:04:44 +0100

    New internal function to get graphNEL through igraph

---
+++ R/as_graphNEL.R
@@ -0,0 +1,17 @@
+#' Transform matrix into graphNEL object
+#'
+#' Internal function to transform an adjacency matrix into a graphNEL object, useful for plotting
+#' @param DAG Adjacency matrix of a DAG
+#'
+#' @return A graphNEL object
+#' @export
+#'
+#' @examples # Randomly generate DAG
+#' q <- 4; w = 0.2
+#' set.seed(123)
+#' DAG <- rDAG(q,w)
+#' as_graphNEL(DAG)
+as_graphNEL <- function(DAG) {
+  graphNEL <- igraph::as_graphnel(as(DAG, "igraph"))
+  return(graphNEL)
+}
